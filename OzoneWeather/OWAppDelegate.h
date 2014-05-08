2f9850177998363ae63a787e24d152aea5a72602
Suz <s.kiihne@gmail.com>
2014-05-08 15:53:11 -0700

    Initial Commit

---
+++ OzoneWeather/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  OzoneWeather
+//
+//  Created by Suzanne Kiihne on 08/05/2014.
+//  Copyright (c) 2014 Suzanne Kiihne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OzoneWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OzoneWeather
+//
+//  Created by Suzanne Kiihne on 08/05/2014.
+//  Copyright (c) 2014 Suzanne Kiihne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OWAppDelegate class]));
+    }
+}
